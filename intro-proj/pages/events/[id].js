69368852207cc1567f530d18a5f24ed2100f239a
arshptl <arshpatel714@gmail.com>
2022-07-13 17:02:58 +0530

    added: selected and filtered event page

--- intro-proj/pages/events/[id].js
+++ intro-proj/pages/events/[id].js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
-const PerticularEvent = () => {
+import { getEventById } from '../../data/dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+
+function EventDetailPage() {
+    const router = useRouter();
+
+    const eventId = router.query.id;
+    const event = getEventById(eventId);
+
+    if (!event) {
+        return (
+            <ErrorAlert>
+                <p>No event found!</p>
+            </ErrorAlert>
+        );
+    }
+
     return (
-        <div>[Displaying Perticular Event]</div>
-    )
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
+    );
 }
 
-export default PerticularEvent
+export default EventDetailPage;
--- intro-proj/pages/index.js
+++ intro-proj/pages/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../data/dummy-data';
+import EventSearch from '../components/events/events-search';
+import { useRouter } from 'next/router';
+
 const HomeScreen = () => {
+
+    const router = useRouter();
+    const events = getFeaturedEvents();
     console.log(getFeaturedEvents());
+
+    function findEventHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`
+        router.push(fullPath);
+    }
     return (
-        <div>
-            <EventList items={getFeaturedEvents()} />
-        </div>
+        <React.Fragment>
+            <EventSearch onSearch={findEventHandler} />
+            <EventList items={events} />
+        </React.Fragment>
     )
 }
 

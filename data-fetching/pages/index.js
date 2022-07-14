e66d68b2b72ab2089cf47e7049ae753de9708731
arshptl <arshpatel714@gmail.com>
2022-07-14 18:58:21 +0530

    worked on getStaticProps  and getStaticPaths

--- data-fetching/pages/index.js
+++ data-fetching/pages/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import Link from 'next/link'
 
 const index = ({ products }) => {
     return (
         <div>
             {products.map((obj) => (
                 <li>
-                    {obj.title}
+                    <Link href={`/${obj.id}`}>
+                        {obj.title}
+                    </Link>
                 </li>
             ))}
         </div>
@@ -14,12 +19,21 @@ const index = ({ products }) => {
 }
 
 export async function getStaticProps() {
+    console.log("Building page.....");
+    // builds an absolute path for that dummy-backend json file
+    const constructPath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonFile = fs.readFileSync(constructPath);
+    const jsonData = JSON.parse(jsonFile);
+
     return {
         props: {
-            products: [{ id: 1, title: 'just a random product' }]
-        }
+            products: jsonData.products,
+        },
+        revalidate: 10,
     }
 }
+
+
 
 export default index
 

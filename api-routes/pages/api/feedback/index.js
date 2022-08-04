26cebb4c49170ba16739c949f9cbd0754859fd09
Harsh Patel <96298315+arshptl@users.noreply.github.com>
2022-08-04 14:27:27 +0530

    Api routes (#2)
    * handle get and post request at server+client side
    
    * then to async await
    
    * dynamic api routes

---
+++ api-routes/pages/api/feedback/index.js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+
+// TODO: Create buildFeedbackPath to get the file location/path
+export const buildFeedbackPath = (apiName) => {
+    const pathh = path.join(process.cwd(), 'data', `${apiName}`);
+    return pathh;
+}
+
+// TODO: Create a func which returns feedback data
+export const feedbackData = (path) => {
+    const file = fs.readFileSync(path);
+    const data = JSON.parse(file);
+    return data;
+}
+
+function handler(req, res) {
+    // Todo get the email and feedback
+    // text and store it into a database
+
+    if (req.method === 'POST') {
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedbackObj = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText
+        }
+
+        // TODO: store this feedback into a database 
+        const filePath = buildFeedbackPath('feedback.json');
+        const data = feedbackData(filePath);
+        data.push(newFeedbackObj);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+        res.status(201).json({ message: 'Success!', feedback: newFeedbackObj })
+    } else {
+        const filePath = buildFeedbackPath('feedback.json');
+        const data = feedbackData(filePath);
+
+        res.status(200).json({
+            feedback: data
+        });
+    }
+}
+
+export default handler
